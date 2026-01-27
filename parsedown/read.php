46276640f029a9183327e01cbf7825a2da1feae6
Hugo Hirsch <github@hugo-hirsch.de>
2026-01-27 15:03:09 +0100

    Read prototype

---
+++ parsedown/read.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "Parsedown.php";
+
+$Parsedown = new Parsedown();
+
+$contents = file_get_contents('data.md');
+if(!$contents) {
+echo $Parsedown->text('Hello _Parsedown_!'); # prints: <p>Hello <em>Parsedown</em>!</p>
+} else {
+
+echo $Parsedown->text($contents);
+}
+?>
