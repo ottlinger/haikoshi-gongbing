af6fa9c1fc3031aad5625574a331e253f416fe59
Hugo Hirsch <github@hugo-hirsch.de>
2026-02-08 22:01:14 +0100

    Reformat

--- parsedown/read.php
+++ parsedown/read.php
@@ -7,10 +7,10 @@ $Parsedown = new Parsedown();
 echo '<a href="/write.php">Editieren</a><br /><hr /><br />';
 
 $contents = file_get_contents('data.md');
-if(!$contents) {
-echo $Parsedown->text('Hello _Parsedown_!'); # prints: <p>Hello <em>Parsedown</em>!</p>
+if (!$contents) {
+    echo $Parsedown->text('Hello _Parsedown_!'); # prints: <p>Hello <em>Parsedown</em>!</p>
 } else {
 
-echo $Parsedown->text($contents);
+    echo $Parsedown->text($contents);
 }
 ?>
--- parsedown/write.php
+++ parsedown/write.php
@@ -6,41 +6,40 @@ $targetFile = 'data.md';
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['contents'])) {
 
 // SAVING A BACKUP of current state
-echo 'Sending old state via mail ....<br />';
-
+    echo 'Sending old state via mail ....<br />';
 
 
 // FLUSHING
-echo 'Flushing .....<br />';
-$content = $_POST['contents']; 
+    echo 'Flushing .....<br />';
+    $content = $_POST['contents'];
 
-$result = file_put_contents($targetFile, $content);
+    $result = file_put_contents($targetFile, $content);
 
-if(!$result) {
-echo 'Error during write - try again';
-} else {
+    if (!$result) {
+        echo 'Error during write - try again';
+    } else {
 // echo '<i>'.$content.'</i><br />';
-echo 'Flushed '.$result.' bytes<br />';
-echo 'Return to <a href="/read.php">user view</a>';
-}
+        echo 'Flushed ' . $result . ' bytes<br />';
+        echo 'Return to <a href="/read.php">user view</a>';
+    }
 
 } else {
-$contents = file_get_contents($targetFile);
+    $contents = file_get_contents($targetFile);
 
-echo 'Hilfe, was ist <a href="https://www.markdownguide.org/cheat-sheet/" target="_blank">Markdown</a> nochmal .....<br /><hr /><br />';
-echo '<form action="/write.php" method="post">';
-echo '<label for="contents">Schreib hier rein, was Du Dir speichern willst:</label><br />';
-echo '<textarea id="contents" name="contents" rows="50" cols="70">';
+    echo 'Hilfe, was ist <a href="https://www.markdownguide.org/cheat-sheet/" target="_blank">Markdown</a> nochmal .....<br /><hr /><br />';
+    echo '<form action="/write.php" method="post">';
+    echo '<label for="contents">Schreib hier rein, was Du Dir speichern willst:</label><br />';
+    echo '<textarea id="contents" name="contents" rows="50" cols="70">';
 
-if(!$contents) {
-echo "Dein Markdown hier";
-} else {
-echo $contents;
-} // no contents
+    if (!$contents) {
+        echo "Dein Markdown hier";
+    } else {
+        echo $contents;
+    } // no contents
 
-echo '</textarea><br /><br />';
-echo '<input type="submit" value="Aktualisieren">';
-echo '</form>';
+    echo '</textarea><br /><br />';
+    echo '<input type="submit" value="Aktualisieren">';
+    echo '</form>';
 
 } // POST
 ?>
