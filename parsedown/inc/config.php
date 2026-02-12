f64ed4a75ef900d883ef569643b74776f3468971
Hugo Hirsch <github@hugo-hirsch.de>
2026-02-12 16:31:11 +0100

    #1: Add info about configuration options

---
+++ parsedown/inc/config.php
@@ -0,0 +1,7 @@
+<?php
+// https://www.php.net/manual/en/reserved.variables.globals.php
+[haikoshi]
+;;// sender mail address
+sender = 'hostmaster@aiki-it.de';
+;;// where to send editor data to
+recipient = 'p@gongbing.de';
