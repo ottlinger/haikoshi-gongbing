01c845a763769697f23f32590db776e58cac276c
Hugo Hirsch <github@hugo-hirsch.de>
2026-02-12 21:20:57 +0100

    #5: Configure fileName

--- parsedown/inc/config.php
+++ parsedown/inc/config.php
@@ -2,6 +2,8 @@
 ;die(); // For further security
 /*
 [haikoshi]
+;;// configure name of data file here, should end with .php in order to be protected
+fileName = 'data.md';
 ;;// sender mail address
 sender = 'hostmaster@aiki-it.de';
 ;;// where to send editor data to
