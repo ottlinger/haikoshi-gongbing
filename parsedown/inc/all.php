5ae129ca10714e4973b1cc7defa2e2544683ff43
Hugo Hirsch <github@hugo-hirsch.de>
2026-02-13 17:46:04 +0100

    #5: Fix parsing/IO error when calling inc scripts directly due to path calculations

--- parsedown/inc/all.php
+++ parsedown/inc/all.php
@@ -1,7 +1,8 @@
 <?php
 // only import this file once in your PHP script in root directory
-require_once "inc/read-config.php";
-require_once "inc/layout.php";
-require_once "inc/mailer.php";
+require_once dirname(__FILE__) . "/../Parsedown.php";
+require_once dirname(__FILE__) . "/../inc/read-config.php";
+require_once dirname(__FILE__) . "/../inc/layout.php";
+require_once dirname(__FILE__) . "/../inc/mailer.php";
 
 require_once dirname(__FILE__) . "/../Parsedown.php";
