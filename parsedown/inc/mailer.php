9c1b5ac1212b5f1e9b24f8d867e419d4fb08f0f4
Hugo Hirsch <github@hugo-hirsch.de>
2026-02-15 22:26:52 +0100

    #1: Compose mail

--- parsedown/inc/mailer.php
+++ parsedown/inc/mailer.php
@@ -1,7 +1,5 @@
 <?php
 
-use haikoshigongbing\FormHelper;
-
 /**
  * Sends out the data file read from disc. All parameters refer to the configuration options configured via {@link config.php}
  * @return void
@@ -9,66 +7,15 @@ use haikoshigongbing\FormHelper;
  */
 function sendAsMail(): void
 {
-    echo "<pre>Sending to " . getFromConfiguration('recipient') . "</pre>";
-
-}
-
-function _createCommonHeaders(): string
-{
-    $serverName = 'localhost';
-    $header = 'MIME-Version: 1.0' . "\r\n";
-    $header .= "Content-Type: text/html; charset=\"utf-8\"\r\n" . "Content-Transfer-Encoding: 8bit\r\n";
-    $header .= 'From: Haikoshi Gongbing <' . getFromConfiguration('sender') . '>' . "\r\n";
-    $header .= 'X-Mailer: Mailform-PHP/' . phpversion() . "\r\n";
-    $header .= 'Message-ID: <' . time() . rand(1, 1000) . '_' . date('YmdHis') . '@' . $serverName . '>' . "\r\n";
-
-    return $header;
-}
-
-function getMailText(): string
-{
-    $timestamp = date('Y-m-d H:i:s');
-    $subjectLine = '[Haikoshi-Gongbing] Datensicherung vom ' . $timestamp;
-
-    $userAgent = 'none';
-    if (FormHelper::isSetAndNotEmpty('HTTP_USER_AGENT')) {
-        $userAgent = FormHelper::filterUserInput($_SERVER['HTTP_USER_AGENT']);
+    if (file_exists(getFromConfiguration("dataFileName"))) {
+        echo "<pre>Sending to " . getFromConfiguration('recipient') . "</pre>";
+        $contents = file_get_contents(getFromConfiguration("dataFileName"));
+        $msg = new \haikoshigongbing\DataCopyMessage($contents);
+    } else {
+        echo "<h2>No content found yet - this is your first edit. ✨ Congratulations!</h2>";
     }
 
-    $remoteAddress = 'none';
-    if (FormHelper::isSetAndNotEmpty('REMOTE_ADDR')) {
-        $remoteAddress = FormHelper::filterUserInput($_SERVER['REMOTE_ADDR']);
-    }
 
-    return "<html lang='en'><head><title>".$subjectLine.'</title></head>
-            <body><h1>'.$subjectLine.'</h1>
-              <table>
-               <tr>
-               <td><b>Time:</b></td>
-               <td>'.$timestamp.'</td>
-               </tr>
-               <tr>
-               <td><b>Name:</b></td>
-               <td>'.$this->_message->getName().'</td>
-               </tr>
-               <tr>
-               <td><b>Message:</b></td>
-               <td>'.$this->_message->getContents().'</td>
-               </tr>
-               <tr>
-               <td><b>Caller-IP:</b></td>
-               <td>'.$remoteAddress.'</td>
-               </tr>
-               <tr>
-               <td><b>Caller-Agent:</b></td>
-               <td>'.$userAgent.'</td>
-               </tr>
-               <tr>
-               <td><b>E-Mail:</b></td>
-               <td>'.strval($this->_message->getEmail()).'</td>
-               </tr>
-              </table>
-            </body>
-            </html>';
 }
+
 
--- parsedown/src/haikoshigongbing/DataCopyMessage.php
+++ parsedown/src/haikoshigongbing/DataCopyMessage.php
@@ -7,6 +7,8 @@ class DataCopyMessage
     private Email $recipient;
     private Email $sender;
     private string $plainContents;
+    private string $timestamp;
+    private string $subjectLine;
 
     /**
      * @param string $plainContents
@@ -14,8 +16,10 @@ class DataCopyMessage
     public function __construct(string $plainContents)
     {
         $this->plainContents = $plainContents;
-        $this->recipient = Email::fromString($plainContents);
-        $this->sender = Email::fromString($plainContents);
+        $this->recipient = Email::fromString(getFromConfiguration('recipient'));
+        $this->sender = Email::fromString(getFromConfiguration('sender'));
+        $this->timestamp = date('Y-m-d H:i:s');
+        $this->subjectLine = '[Haikoshi-Gongbing] Datensicherung vom ' . $this->timestamp;
     }
 
     public function getRecipient(): Email
@@ -28,5 +32,68 @@ class DataCopyMessage
         return $this->sender;
     }
 
+    public function getTimestamp(): string
+    {
+        return $this->timestamp;
+    }
+
+    public function getSubjectLine(): string
+    {
+        return $this->subjectLine;
+    }
+
+    public function getPlainContents(): string
+    {
+        return $this->plainContents;
+    }
+
+
+
+    function _createCommonHeaders(): string
+    {
+        $serverName = 'localhost';
+        $header = 'MIME-Version: 1.0' . "\r\n";
+        $header .= "Content-Type: text/html; charset=\"utf-8\"\r\n" . "Content-Transfer-Encoding: 8bit\r\n";
+        $header .= 'From: Haikoshi Gongbing <' . $this->getSender() . '>' . "\r\n";
+        $header .= 'X-Mailer: HaikoshiGongbing-' . phpversion() . "\r\n";
+        $header .= 'Message-ID: <' . time() . rand(1, 1000) . '_' . date('YmdHis') . '@' . $serverName . '>' . "\r\n";
+        return $header;
+    }
+
+    function getMailText(): string
+    {
+        $userAgent = 'none';
+        if (FormHelper::isSetAndNotEmpty('HTTP_USER_AGENT')) {
+            $userAgent = FormHelper::filterUserInput($_SERVER['HTTP_USER_AGENT']);
+        }
+
+        $remoteAddress = 'none';
+        if (FormHelper::isSetAndNotEmpty('REMOTE_ADDR')) {
+            $remoteAddress = FormHelper::filterUserInput($_SERVER['REMOTE_ADDR']);
+        }
+
+        return '<html lang="en"><head><title>Haikoshi-Datensicherung</title></head>
+            <body><h1>' . $this->getSubjectLine() . '</h1>
+              <table>
+               <tr>
+               <td><b>Time:</b></td>
+               <td>' . $this->getTimestamp() . '</td>
+               </tr>
+               <tr>
+               <td><b>Inhalt:</b></td>
+               <td>' . $this->getPlainContents() . '</td>
+               </tr>
+               <tr>
+               <td><b>Caller-IP:</b></td>
+               <td>' . $remoteAddress . '</td>
+               </tr>
+               <tr>
+               <td><b>Caller-Agent:</b></td>
+               <td>' . $userAgent . '</td>
+               </tr>
+              </table>
+            </body>
+            </html>';
+    }
 
 }
