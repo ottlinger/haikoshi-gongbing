2c0521eb90b21f47147717e80f2effce3fb49945
Hugo Hirsch <github@hugo-hirsch.de>
2026-02-16 16:04:07 +0100

    #6: continue tests

--- tests/haikoshigongbing/DataCopyMessageTest.php
+++ tests/haikoshigongbing/DataCopyMessageTest.php
@@ -6,24 +6,25 @@ use PHPUnit\Framework\TestCase;
 
 class DataCopyMessageTest extends TestCase
 {
-    public function test__construct()
+    public function testConstruct()
     {
         $this->assertEquals('plainContentsInTest', new DataCopyMessage('plainContentsInTest')->getPlainContents());
     }
 
     public function testGetMailText()
     {
-
+        $this->assertNotEmpty(DataCopyMessage::getMailText());
     }
 
     public function testGetTimestamp()
     {
-
+        $this->assertNotEmpty(DataCopyMessage::getTimestamp());
     }
 
     public function testGetSender()
     {
-
+        // is empty due to missing configuration
+        $this->assertEmpty(new DataCopyMessage('sender')->getSender());
     }
 
     public function testSend()
@@ -38,16 +39,18 @@ class DataCopyMessageTest extends TestCase
 
     public function testGetSubjectLine()
     {
-
+        // is empty due to missing configuration
+        $this->assertEmpty(new DataCopyMessage('subjectLine')->getSubjectLine());
     }
 
     public function test_createCommonHeaders()
     {
-
+        $this->assertNotEmpty(new DataCopyMessage('headers')->_createCommonHeaders());
     }
 
     public function testGetRecipient()
     {
-
+        // is empty due to missing configuration
+        $this->assertNotEmpty(new DataCopyMessage('recipient')->getRecipient());
     }
 }
