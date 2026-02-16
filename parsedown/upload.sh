7605a2abc8f30ea3237a723e865548d539162711
Hugo Hirsch <github@hugo-hirsch.de>
2026-02-16 10:51:41 +0100

    #6: Move one dir up

--- parsedown/upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Source and destination directories
-SOURCE_DIR="."
-DEST_DIR="./upload"
-# Cleanup first
-rm -rf "$DEST_DIR"
-mkdir -p "$DEST_DIR"
-# Copy files, excluding specific patterns and do not OVERRIDE existing data
-rsync -av --exclude='.phpunit.cache' --exclude='.idea' --exclude='coverage*' --exclude='testdox*' --exclude='phpunit*' --exclude='tests' --exclude='upload' --exclude='*.sh' --exclude='vendor' --exclude='*.md.php' --exclude='*.md' --exclude='composer*' --exclude='docker-compose*' --exclude='LICENSE*' --exclude='*.iml' --exclude='*example' "$SOURCE_DIR/" "$DEST_DIR/"
