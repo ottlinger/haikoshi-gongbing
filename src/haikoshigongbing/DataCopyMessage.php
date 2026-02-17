7831cb4eebccdffa42bcd18a3c808ea97d02aa68
Hugo Hirsch <github@hugo-hirsch.de>
2026-02-17 14:27:54 +0100

    #1: Try to attach file

--- src/haikoshigongbing/DataCopyMessage.php
+++ src/haikoshigongbing/DataCopyMessage.php
@@ -2,7 +2,7 @@
 
 namespace haikoshigongbing;
 
-require_once dirname(__FILE__).'/../../inc/read-config.php';
+require_once dirname(__FILE__) . '/../../inc/read-config.php';
 
 class DataCopyMessage
 {
@@ -12,11 +12,12 @@ class DataCopyMessage
     private string $timestamp;
     private string $subjectLine;
     private bool $send;
+    private string $separator;
     private static string $lineBreak = "\r\n";
 
     /**
      * @param string $plainContents
-     * @param bool   $send
+     * @param bool $send
      */
     public function __construct(string $plainContents, bool $send = false)
     {
@@ -24,8 +25,9 @@ class DataCopyMessage
         $this->recipient = Email::fromString(getFromConfiguration('recipient'));
         $this->sender = Email::fromString(getFromConfiguration('sender'));
         $this->timestamp = date('Y-m-d H:i:s');
-        $this->subjectLine = '[Haikoshi-Gongbing] Datensicherung vom '.$this->timestamp;
+        $this->subjectLine = '[Haikoshi-Gongbing] Datensicherung vom ' . $this->timestamp;
         $this->send = $send;
+        $this->separator = md5(time());
     }
 
     public function getRecipient(): Email
@@ -37,6 +39,12 @@ class DataCopyMessage
     {
         return $this->sender;
     }
+
+    public function getSeparator(): string
+    {
+        return $this->separator;
+    }
+
 
     public function getTimestamp(): string
     {
@@ -59,7 +67,7 @@ class DataCopyMessage
             $header = $this->_createCommonHeaders();
             $success = mail(strval($this->getRecipient()), $this->getSubjectLine(), $this->getMailText(), $header);
             if (!$success) {
-                echo '<p>Error message: '.error_get_last()['message'].'</p>';
+                echo '<p>Error message: ' . error_get_last()['message'] . '</p>';
             }
 
             return $success;
@@ -73,14 +81,16 @@ class DataCopyMessage
     public function _createCommonHeaders(): string
     {
         $serverName = 'localhost';
-        $header = 'MIME-Version: 1.0'.self::$lineBreak;
-        $header .= 'Content-Type: text/html; charset="utf-8"'.self::$lineBreak;
-        $header .= 'Content-Transfer-Encoding: 8bit'.self::$lineBreak;
-        $header .= 'From: Haikoshi Gongbing 🤖 <'.$this->getSender().'>'.self::$lineBreak;
-        $header .= 'X-Mailer: HaikoshiGongbing-v'.phpversion().self::$lineBreak;
-        $header .= 'Message-ID: <'.time().rand(1, 1000).'_'.date('YmdHis').'@'.$serverName.'>'.self::$lineBreak;
+        $headers = 'MIME-Version: 1.0' . self::$lineBreak;
+        //$headers .= 'Content-Type: text/html; charset="utf-8"'.self::$lineBreak;
+        $headers .= "Content-Type: multipart/mixed; boundary=\"" . $this->getSeparator() . "\"" . self::$lineBreak;
+        $headers .= 'Content-Type: text/html; charset="utf-8"' . self::$lineBreak;
+        $headers .= 'Content-Transfer-Encoding: 8bit' . self::$lineBreak;
+        $headers .= 'From: Haikoshi Gongbing 🤖 <' . $this->getSender() . '>' . self::$lineBreak;
+        $headers .= 'X-Mailer: HaikoshiGongbing-v' . phpversion() . self::$lineBreak;
+        $headers .= 'Message-ID: <' . time() . rand(1, 1000) . '_' . date('YmdHis') . '@' . $serverName . '>' . self::$lineBreak;
 
-        return $header;
+        return $headers;
     }
 
     public function getMailText(): string
@@ -95,26 +105,45 @@ class DataCopyMessage
             $remoteAddress = FormHelper::filterUserInput($_SERVER['REMOTE_ADDR']);
         }
 
-        return '<html lang="en"><head><title>🤖 Haikoshi-Datensicherung</title></head>
-            <body><h1>'.$this->getSubjectLine().'</h1>
+        $dataFile = getFromConfiguration("dataFileName");
+        $attachmentContent = chunk_split(base64_encode(file_get_contents($dataFile)));
+        $attachmentName = basename($dataFile);
+
+        $message = '<html lang="en"><head><title>🤖 Haikoshi-Datensicherung</title></head>
+            <body><h1>' . $this->getSubjectLine() . '</h1>
               <table>
                <tr>
                <td><b>Time:</b></td>
-               <td>'.$this->getTimestamp().'</td>
+               <td>' . $this->getTimestamp() . '</td>
                </tr>
                <tr>
                <td><b>Caller-IP:</b></td>
-               <td>'.$remoteAddress.'</td>
+               <td>' . $remoteAddress . '</td>
                </tr>
                <tr>
                <td><b>Caller-Agent:</b></td>
-               <td>'.$userAgent.'</td>
+               <td>' . $userAgent . '</td>
                </tr>
               </table>
               <p>
-              <code>'.$this->getPlainContents().'</code>
+              <code>' . $this->getPlainContents() . '</code>
               </p>
             </body>
             </html>';
+
+        // compose body with message and attachment
+        $body = "--" . $this->getSeparator() . self::$lineBreak;
+        $body .= "Content-Type: text/plain; charset=\"utf-8\"" . self::$lineBreak;
+        $body .= "Content-Transfer-Encoding: 7bit" . self::$lineBreak . self::$lineBreak;
+        $body .= $message . self::$lineBreak;
+        $body = "--" . $this->getSeparator() . self::$lineBreak;
+        $body .= "Content-Type: application/octet-stream; name=\"" . $attachmentName . "\"" . self::$lineBreak;
+        $body .= "Content-Transfer-Encoding: base64" . self::$lineBreak;
+        $body .= "Content-Disposition: attachment; filename=\"" . $attachmentName . "\"" . self::$lineBreak . self::$lineBreak;
+        $body .= $attachmentContent . self::$lineBreak;
+        $body .= "--" . $this->getSeparator() . self::$lineBreak . "--";
+
+        return $body;
     }
+
 }
